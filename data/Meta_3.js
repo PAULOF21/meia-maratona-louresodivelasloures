3ee0b13a0527bb243cc3600479bd2f0634210b14
PAULOF21 <paulof21@live.com.pt>
2024-07-22 13:16:27 +0100

    Add files via upload

---
+++ data/Meta_3.js
@@ -0,0 +1,1 @@
+var json_Meta_3 = {"type":"FeatureCollection","name":"Meta_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"Tipo":"Meta"},"geometry":{"type":"Point","coordinates":[-9.161487246782405,38.825158647204468]}}]}
