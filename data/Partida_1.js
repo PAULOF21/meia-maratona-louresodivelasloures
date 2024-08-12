fbbe5183b133b1c1dbdc3b07ad8380416f2bcf8d
PAULOF21 <paulof21@live.com.pt>
2024-08-12 16:21:03 +0100

    Add files via upload

---
+++ data/Partida_1.js
@@ -0,0 +1,1 @@
+var json_Partida_1 = {"type":"FeatureCollection","name":"Partida_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"Tipo":"Partida"},"geometry":{"type":"Point","coordinates":[-9.16153088919072,38.825088301981047]}}]}
