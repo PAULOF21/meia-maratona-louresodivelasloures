1eed3316f49a47e063f67fd91d7f49d38b0b329e
PAULOF21 <paulof21@live.com.pt>
2024-09-10 18:05:13 +0100

    Add files via upload

---
+++ data/AbastecimentoCaminhada_8.js
@@ -0,0 +1,1 @@
+var json_AbastecimentoCaminhada_8 = {"type":"FeatureCollection","name":"AbastecimentoCaminhada_8","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":null,"Tipo":"Abastecimento Caminhada"},"geometry":{"type":"Point","coordinates":[-9.152190943525612,38.83529496032763]}}]}
